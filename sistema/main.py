cb47c3a0da88c01327ecff2cd16edb91738a254b
jmleandro <137917668+jmleandro@users.noreply.github.com>
2023-12-15 10:43:47 -0300

    Add files via upload

---
+++ sistema/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, render_template, redirect, url_for
+import mysql.connector
+
+app = Flask(__name__)
+
+# Configurações do banco de dados
+db_username = 'root'
+db_password = 'senha1'
+db_name = 'zgsistemas'
+
+# Conectar ao banco de dados MySQL
+db = mysql.connector.connect(
+    host='localhost',
+    port=3307,
+    user=db_username,
+    password=db_password,
+    database=db_name
+)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    
+    cursor = db.cursor()
+    query = "SELECT * FROM users WHERE email = %s AND password = %s"
+    cursor.execute(query, (email, password))
+    user = cursor.fetchone()
+
+    if user:
+        # Login bem-sucedido
+        return redirect(url_for('dashboard'))  # Redireciona para a página de dashboard após o login
+    else:
+        # Login falhou
+        return "Credenciais inválidas. Tente novamente."
+
+@app.route('/dashboard')
+def dashboard():
+    # Página de dashboard após o login bem-sucedido
+    return "Você está logado! Esta é a página de dashboard."
+
+# cadastro
+@app.route('/cadastrar')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/cadastrousu')
+def cadastro_usuario():
+    email = request.form['email']
+    password = request.form['password']
+    
+    cursor = db.cursor()
+    query = "INSERT INTO users (password, email) VALUES (%s, %s)"
+    cursor.execute(query, (password, email))
+    user = cursor.fetchone()
+
+    return "porque é obrigado"
+    
+if __name__ == '__main__':
+    app.run(debug=True)
