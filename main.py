a2b6c0845bc4dec718ba8c505c813fcfce467303
Lucas Guandalin <106684353+Guandalfi@users.noreply.github.com>
2023-12-18 11:42:09 -0300

    Update main.py
    Mudei o nome de algumas variaveis para ficar mais facil de enteder mais tarde

--- main.py
+++ main.py
@@ -3,73 +3,72 @@ import mysql.connector
 
 app = Flask(__name__)
 
-db_username = 'root'
-db_password = 'senha1'
-db_name = 'zgsistemas'
+db_username = "root"
+db_password = "senha1"
+db_name = "zgsistemas"
+db_port = 3307
 
 db = mysql.connector.connect(
-    host='192.168.28.11',
+    host="192.168.28.21",
     port=3307,
     user=db_username,
     password=db_password,
-    database=db_name
+    database=db_name,
+    autocommit=True,
+    connection_timeout=10
 )
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/login', methods=['POST'])
+@app.route("/login", methods=["POST"])
 def login():
-    email = request.form['email']
-    password = request.form['password']
+    email = request.form["email"]
+    password = request.form["password"]
     if email == '' or password == '':
-        return render_template('index.html', output="Email ou Senha em branco")
+        return render_template("index.html", output="Email ou Senha em branco")
     
     cursor = db.cursor()
-    query = "SELECT * FROM users WHERE email = %s AND password = %s"
-    cursor.execute(query, (email, password))
-    user = cursor.fetchone()
+    busca_credenciais = "SELECT * FROM users WHERE email = %s AND password = %s"
+    cursor.execute(busca_credenciais, (email, password))
+    usuario = cursor.fetchone()
     cursor.close()
     
-    if user:
-        # Login bem-sucedido
-        return redirect(url_for('dashboard'))  # Redireciona para a página de dashboard após o login
+    if usuario != None:
+        return redirect(url_for("dashboard"))
     else:
-        # Login falhou
         return "Credenciais inválidas. Tente novamente."
 
-@app.route('/dashboard', methods=['GET'])
+@app.route("/dashboard", methods=["GET"])
 def dashboard():
     return "Você está logado! Esta é a página de dashboard."
 
 # cadastro
-@app.route('/cadastrar', methods=['GET'])
+@app.route("/cadastrar", methods=["GET"])
 def cadastro():
-    return render_template('cadastro.html')
+    return render_template("cadastro.html")
 
-@app.route('/cadastrousu', methods=['POST'])
+@app.route("/cadastrausuario", methods=["POST"])
 def cadastro_usuario():
-    email = request.form['email']
-    password = request.form['password']
+    email = request.form["email"]
+    password = request.form["password"]
     if email == '' or password == '':
-        return render_template('cadastro.html', output="Email ou Senha em branco")
+        return render_template("cadastro.html", output="Email ou Senha em branco")
     
     cursor = db.cursor()
-    verifica_email ='Select email from users where email = %s'
-    cursor.execute(verifica_email, (email,))
-    var = cursor.fetchone()
+    busca_email ="Select email from users where email = %s"
+    cursor.execute(busca_email, (email,))
+    email_usuario = cursor.fetchone()
 
-    if var != None:
-        return render_template('cadastro.html', output="Ja tem cadastrado")
-    
-    print(var)
-    query = 'INSERT INTO users (password, email) VALUES (%s, %s)'
-    cursor.execute(query, (password, email))
-    db.commit()
-    cursor.close()
+    if email_usuario != None:
+        return render_template("cadastro.html", output="Email já cadastrado !")
+    else:
+        insere_usuario = "INSERT INTO users (password, email) VALUES (%s, %s)"
+        cursor.execute(insere_usuario, (password, email))
+        cursor.close()
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
     
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
